64b6f45902b74274f3b0840109d232f26403ad2d
Tony-sg <397601798@qq.com>
2017-03-22 01:15:26 +0800

    init project

---
+++ HelloGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloGit
+//
+//  Created by Tony-sg on 17/3/22.
+//  Copyright © 2017年 Tony-sg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HelloGit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloGit
+//
+//  Created by Tony-sg on 17/3/22.
+//  Copyright © 2017年 Tony-sg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
